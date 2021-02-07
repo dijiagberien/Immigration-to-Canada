c6a67fa11280c2d7b915e8e7775fca927f91ef5d
dijiagberien <diji_agberien@yahoo.com>
2021-02-07 00:22:21 -0500

    Data preprocessing, version 1

---
+++ script.one.R
@@ -0,0 +1,45 @@
+######################################### IMMIGRATION TO CANADA #########################################
+# Clear working directory
+rm(list = ls())
+
+# Import and load packages
+required_packages <- c("data.table", "tidyverse")
+
+packageCheck <- lapply(required_packages, FUN = function(x) {
+  if(!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+    library(x, character.only = TRUE)
+  }
+})
+
+# Set working directory 
+#setwd("") Input your working directory here.
+
+# Import the dataset
+canada.immigration.data.raw <- data.table::fread("Canada.csv", skip = 1)
+
+# Data Preprocessing 
+# Drop the unrequired columns and rows. Make sure to check your data during this step, mine imported with NA columns
+canada.immigration.data.clean <- canada.immigration.data.raw %>%
+  dplyr::select(c(-44:-51), -Coverage, -Type, -AREA, -REG, -DEV) %>% 
+  .[1:197, ]
+
+# Reshape the data 
+canada.immigration.data.clean <- melt(canada.immigration.data.clean,
+                                      id.vars = 1:4, measure.vars  = 5:38,
+                                      variable.name = "Immigration Year",
+                                      value.name = "Immigrant Count") %>% 
+  setnames(old = c("OdName", "AreaName", "RegName", "DevName"),
+           new = c("Country", "Continent", "Sub-continent", "Development status")) 
+
+# Drop the country named "Total"
+canada.immigration.data.clean <- canada.immigration.data.clean[Country != "Total"]
+
+# Check the presence of missing data
+sum(is.na(canada.immigration.data.clean))
+
+# Convert the year from factor to numeric 
+canada.immigration.data.clean[, `Immigration Year` := as.numeric(as.character(`Immigration Year`))]
+
+# Export data (to be used for data visualization in tableau)
+fwrite(canada.immigration.data.clean, "canada.immigration.data.clean.csv")
